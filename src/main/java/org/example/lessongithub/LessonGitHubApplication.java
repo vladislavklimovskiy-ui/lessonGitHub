a61259b33e4d5a76a39e75b022e7965b693bbda3
vladislavklimovskiy-ui <vladislav.klimovskiy@gmail.com>
2026-08-17 22:46:17 +0500

    Initial commit

---
+++ src/main/java/org/example/lessongithub/LessonGitHubApplication.java
@@ -0,0 +1,14 @@
+package org.example.lessongithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonGitHubApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/example/lessongithub/LessonGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lessongithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
