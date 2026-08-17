5d51b0d68726ddbc86ad61b4453027c2d01ebf33
vladislavklimovskiy-ui <vladislav.klimovskiy@gmail.com>
2026-08-17 22:54:02 +0500

    добавлен пользователь

---
+++ src/main/java/org/example/lessongithub/User.java
@@ -0,0 +1,31 @@
+package org.example.lessongithub;
+
+public class User {
+   private String name;
+  private   int balance;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
